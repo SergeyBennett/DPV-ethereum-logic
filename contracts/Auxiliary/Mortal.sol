4eb540a5a266b22552ceb3df63396122164751e7
sergeybennett <sergeybennett@gmail.com>
2018-03-15 21:41:14 +0200

    Add auxiliary contracts

---
+++ contracts/Auxiliary/Mortal.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+import "Owned.sol";
+
+contract Mortal is Owned {
+    // This contract inherits the `onlyOwner` modifier from
+    // `owned` and applies it to the `close` function, which
+    // causes that calls to `close` only have an effect if
+    // they are made by the stored owner.
+    function close() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
